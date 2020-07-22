4b1ea47c3a78a42fc15382e2497c2ecf08ce82ed
WMDA <p3dh0001@gmail.com>
2020-07-22 13:25:55 +0100

    updated td_k_value.py

--- td_k_value.py
+++ td_k_value.py
@@ -8,10 +8,24 @@ Created on Tue Jul 21 18:14:00 2020
 
 import os
 import pandas as pd
+import numpy as np
+import scipy.optimize as sp 
+import math
 
-os.chdir('')
 
-df = pd.read_csv('')
+
+'''
+Currently does not as code has not been completed, please do not run 
+untill completed. 
+'''
+
+def find_files():
+    #Works on my system at home but needs further testing.
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if 'Tiara_DelayDiscount_RespFiles' in root:
+                return root
 
 def simulatechoice(k,sir,ldr,delay):
     value = (ldr/(1 +k*delay))-sir
@@ -19,3 +33,19 @@ def simulatechoice(k,sir,ldr,delay):
         return 1
     else:
         return 0
+
+def getPchoice(k,sir,ldr,delay,realchoice):
+    if realchoice==1:
+        p_choice=mat.exp()
+
+def generateloglik(cur_k,length):
+    choiceprobabilities= np.zeros(len(choices))
+    for i in choiceprobabilities:
+        choiceprobabilities= 
+
+root=find_files()
+
+for i in root:
+    if "forfit" in i:
+        df = read.csv(i)
+        x,y=sp.fmin(generateloglik())
